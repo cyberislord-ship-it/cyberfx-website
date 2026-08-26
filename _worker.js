418b8a02f064f02b2bcb4a7154c46bc7b5fa98c2
cyberislord-ship-it <cyberislord@gmail.com>
2026-08-26 11:12:04 +0100

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -2,13 +2,21 @@ export default {
   async fetch(request, env) {
     const url = new URL(request.url);
 
-    if (url.pathname === "/api/test-key") {
+    if (url.pathname === "/api/test-twelve") {
+      const testUrl = new URL("https://api.twelvedata.com/price");
+      testUrl.searchParams.set("symbol", "XAU/USD");
+      testUrl.searchParams.set("apikey", env.TWELVE_DATA_API_KEY);
+
+      const response = await fetch(testUrl);
+      const data = await response.json();
+
       return new Response(
         JSON.stringify({
-          secret_exists: !!env.TWELVE_DATA_API_KEY,
-          secret_length: env.TWELVE_DATA_API_KEY
-            ? env.TWELVE_DATA_API_KEY.length
-            : 0
+          http_status: response.status,
+          twelve_data_status: data.status || null,
+          code: data.code || null,
+          message: data.message || null,
+          has_price: !!data.price
         }),
         {
           headers: {
